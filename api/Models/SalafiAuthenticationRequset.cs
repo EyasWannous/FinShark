64605134c68a9abc2d84637b73f7e09f85aa9fff
Eyas Wannous <eyaswannous@gmail.com>
2024-03-26 20:55:38 +0300

    Delete 'api' folder

--- api/Models/SalafiAuthenticationRequset.cs
+++
@@ -1,7 +0,0 @@
-namespace api.Controllers;
-
-public class SalafiAuthenticationRequset
-{
-    public string UserName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
--- api/options/AttachmentsOptions.cs
+++
@@ -1,9 +0,0 @@
-
-namespace api.options;
-
-public class AttachmentsOptions
-{
-    public string AllowedExtensions { get; set; } = string.Empty;
-    public int MaxSizeInMegaByte { get; set; }
-    public bool EnableCompression { get; set; }
-}
